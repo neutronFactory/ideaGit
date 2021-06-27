8520b947363c7edab13bc035dd780e70ee60fc32
ltq <88480356@qq.com>
2021-06-27 09:49:00 +0800

    2

--- git-test/src/HelloGit.java
+++ git-test/src/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("111111");
         System.out.println("111111");
+        System.out.println("222222");
     }
 }
